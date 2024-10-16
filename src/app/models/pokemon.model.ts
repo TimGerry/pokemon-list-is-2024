2ac9f1179612bce5fb622f5b168a41afe5c90f43
Tim Gerry Geerts <tim.geerts.osv@fedex.com>
2024-10-16 09:52:12 +0200

    Advanced Typescript

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -26,3 +26,43 @@ export type PokemonType =
     | "dark"
     | "steel"
     | "fairy";
+
+function isPokemon(maybePokemon: unknown): maybePokemon is Pokemon {
+    return Boolean(maybePokemon && typeof maybePokemon === 'object'
+        && 'id' in maybePokemon && typeof maybePokemon.id === 'string'
+        && 'name' in maybePokemon && typeof maybePokemon.name === 'string'
+        && 'type' in maybePokemon && typeof maybePokemon.type === 'string' && pokemonTypes.includes(maybePokemon.type)
+        && (!('type2' in maybePokemon) || typeof maybePokemon.type2 === 'string' && pokemonTypes.includes(maybePokemon.type2))
+        && 'attack' in maybePokemon && typeof maybePokemon.attack === 'string'
+        && 'level' in maybePokemon && typeof maybePokemon.level === 'number'
+    );
+}
+
+export function assertIsPokemon(value: unknown): asserts value is Pokemon {
+    if (!isPokemon(value)) throw new Error('This is not a pokémon!');
+}
+
+export function assertIsPokemonArray(value: unknown): asserts value is Pokemon[] {
+    if (!Array.isArray(value) || !value.every(isPokemon)) throw new Error('This is not a pokémon array!');
+}
+
+export const pokemonTypes: string[] = [
+    "normal",
+    "fire",
+    "water",
+    "electric",
+    "grass",
+    "ice",
+    "fighting",
+    "poison",
+    "ground",
+    "flying",
+    "psychic",
+    "bug",
+    "rock",
+    "ghost",
+    "dragon",
+    "dark",
+    "steel",
+    "fairy"
+];
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Pokemon } from '../models/pokemon.model';
+import { assertIsPokemon, assertIsPokemonArray, Pokemon } from '../models/pokemon.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable, OperatorFunction } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 const BASE_URL = environment.baseUrl + '/pokemon'
@@ -14,10 +14,21 @@ export class PokemonService {
   constructor(private httpClient: HttpClient) { }
 
   getAll(): Observable<Pokemon[]> {
-    return this.httpClient.get<Pokemon[]>(BASE_URL);
+    return this.httpClient.get<unknown>(BASE_URL).pipe(
+      assert(assertIsPokemonArray)
+    );
   }
 
   get(id: string): Observable<Pokemon> {
-    return this.httpClient.get<Pokemon>(`${BASE_URL}/${id}`);
+    return this.httpClient.get<unknown>(`${BASE_URL}/${id}`).pipe(
+      assert(assertIsPokemon)
+    );
   }
 }
+
+const assert = <T>(
+  fn: (value: unknown) => asserts value is T
+): OperatorFunction<unknown, T> => map(data => {
+  fn(data);
+  return data;
+});
